f0d0831c8f2639a3de9a31eb7b043d57371f6557
James Warner <james.warner@metoffice.gov.uk>
2026-07-15 14:09:47 +0100

    apply code suggestions

--- src/CSET/operators/plot.py
+++ src/CSET/operators/plot.py
@@ -2519,11 +2519,10 @@ def hinton_plot(change, signif, xaxis_labels, yaxis_labels, magnitude=None):
         y += tri_height
         y_edges.append(y)
 
-   if magnitude:
-       txt_y.append(y + txt_height / 2)
-       y += txt_height
-       y_edges.append(y)
-
+        if magnitude is not None:
+            txt_y.append(y + txt_height / 2)
+            y += txt_height
+            y_edges.append(y)
 
     total_height = y
 
