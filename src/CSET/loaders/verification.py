a29b37f5e46aeb5d512d4f6a450c5b45c7b6b1e0
BernardClaxton <bernie.claxton@metoffice.gov.uk>
2026-07-15 14:47:19 +0100

    Update src/CSET/loaders/verification.py
    Co-authored-by: Sylvia Bohnenstengel <62748926+Sylviabohnenstengel@users.noreply.github.com>

--- src/CSET/loaders/verification.py
+++ src/CSET/loaders/verification.py
@@ -100,7 +100,7 @@ def load(conf: Config):
 
     scores_timeseries_methods = _get_scores_timeseries_methods(conf)
     if scores_timeseries_methods:
-        # Produce timeseries plots of scores metrics averaged over the domain.
+        # Produce timeseries plots of scores metrics averaged over the domain for each case study.
         base_model = models[0]
         for model, field, scores_method in itertools.product(
             models[1:], conf.SURFACE_FIELDS, scores_timeseries_methods
